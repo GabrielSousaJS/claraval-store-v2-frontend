d083fdc562535adbd3e0c75bff70b95e52348938
GabrielSousaJS <gabriels.santosx@gmail.coom>
2023-08-11 20:14:22 -0300

    FIX: responsividade de ProductCard

--- frontend/src/components/ProductCard/index.tsx
+++ frontend/src/components/ProductCard/index.tsx
@@ -2,27 +2,14 @@ import "./styles.css";
 
 import ProductPrice from "../ProductPrice";
 
-export function ProductCard() {
-  const product = {
-    id: 1,
-    name: "iPhone 13 Pro",
-    description:
-      "O mais recente smartphone topo de gama da Apple com um impressionante ecrã Super Retina XDR, chip A15 Bionic, sistema de câmara Pro, conectividade 5G e iOS 15.",
-    price: 6291.3,
-    quantity: 17,
-    imgUrl:
-      "https://images-americanas.b2w.io/produtos/01/00/img/3919423/9/3919423949_1GG.jpg",
-    categories: [
-      {
-        id: 1,
-        name: "Eletrônicos",
-      },
-    ],
-  };
+type Props = {
+  product: any;
+};
 
+export function ProductCard({ product }: Props) {
   return (
     <div className="base-card product-card">
-      <div className="d-flex justify-content-center align-items-center pt-4 pb-4 border-bottom">
+      <div className="text-center border-bottom p-3">
         <img className="w-auto" src={product.imgUrl} alt={product.name} />
       </div>
 
--- frontend/src/routes/ClientHome/Catalog/index.tsx
+++ frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -1,36 +1,72 @@
+import "./styles.css";
+
 import { ProductCard } from "../../../components/ProductCard";
 
 export default function Catalog() {
+  const product = {
+    id: 1,
+    name: "iPhone 13 Pro vamos aumentar o seu nome",
+    description:
+      "O mais recente smartphone topo de gama da Apple com um impressionante ecrã Super Retina XDR, chip A15 Bionic, sistema de câmara Pro, conectividade 5G e iOS 15.",
+    price: 6291.3,
+    quantity: 17,
+    imgUrl:
+      "https://images-americanas.b2w.io/produtos/01/00/img/3919423/9/3919423949_1GG.jpg",
+    categories: [
+      {
+        id: 1,
+        name: "Eletrônicos",
+      },
+    ],
+  };
+
+  const product1 = {
+    id: 1,
+    name: "iPhone 13 Pro",
+    description:
+      "O mais recente smartphone topo de gama da Apple com um impressionante ecrã Super Retina XDR, chip A15 Bionic, sistema de câmara Pro, conectividade 5G e iOS 15.",
+    price: 6291.3,
+    quantity: 17,
+    imgUrl:
+      "https://images-americanas.b2w.io/produtos/01/00/img/3919423/9/3919423949_1GG.jpg",
+    categories: [
+      {
+        id: 1,
+        name: "Eletrônicos",
+      },
+    ],
+  };
+
   return (
     <main className="container pt-4">
-      <h1 className="m-0">Catálogo de produtos</h1>
-      <div className="row pt-4">
+      <h1 className="catalog-title">Catálogo de produtos</h1>
+      <div className="row pt-4 pb-4">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product1} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product1} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard />
+          <ProductCard product={product} />
         </div>
       </div>
     </main>
