bd0f2d9cc0c4cc54d186484d4a16de693e7fc282
Gabriel Santos <gabriels.santosx@gmail.com>
2023-09-15 19:57:57 -0300

    Merge

--- frontend/src/routes/Admin/index.tsx
+++ frontend/src/routes/Admin/index.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import HeaderAdmin from "../../components/HeaderAdmin";
-import Footer from "../../components/Footer";
 
 export default function Admin() {
   return (
     <>
       <HeaderAdmin />
       <Outlet />
-      <Footer />
     </>
   );
 }
--- frontend/src/routes/ClientHome/index.tsx
+++ frontend/src/routes/ClientHome/index.tsx
@@ -1,4 +1,3 @@
-import Footer from "../../components/Footer";
 import HeaderClient from "../../components/HeaderClient";
 import { Outlet } from "react-router-dom";
 
@@ -7,7 +6,6 @@ export default function ClientHome() {
     <>
       <HeaderClient />
       <Outlet />
-      <Footer />
     </>
   );
 }
