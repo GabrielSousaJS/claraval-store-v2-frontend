af87c9ee8408541cb0215e2e52c6d023f16f9ad5
Gabriel Santos <gabriels.santosx@gmail.com>
2024-03-06 23:38:44 -0300

    Executar como localhost

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,7 +1,7 @@
-export const BASE_URL = "https://claraval-store-148d38f37df5.herokuapp.com";
+export const BASE_URL = "http://localhost:8080";
 
-export const CLIENT_ID = "lasdghfjrpqoewuryqweoiryuifhkjxvbnvxvzxmcnvbzxmcnvbaf";
+export const CLIENT_ID = "myclientid";
 
-export const CLIENT_SECRET = "hlasdkjfhoqpeuwyrpqweuirtyoweriuhgflhjksdbfhjasdgfvmzbvnmzxbcvkagsdkfhjglfaoqeuirypqriufkjfhjg";
+export const CLIENT_SECRET = "myclientsecret";
 
 export const TOKEN_KEY = "com.claravalstore/Token";
