8586dc8eb3a6be147a7c234fe26f9e96e163f1e3
Gabriel Santos <106276089+GabrielSousaJS@users.noreply.github.com>
2024-03-14 19:33:34 -0300

    Variáveis de ambiente para produção

--- frontend/src/utils/system.ts
+++ frontend/src/utils/system.ts
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const CLIENT_ID = "myclientid";
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
 
-export const CLIENT_SECRET = "myclientsecret";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
 
 export const TOKEN_KEY = "com.claravalstore/Token";
